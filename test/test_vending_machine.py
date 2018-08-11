ab951b864c65b9b6ae2a16ee62104912ffd66b75
Mike Brown <mike.brown@excella.com>
2018-08-10 20:09:08 -0400

    update to pytest

--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -1,28 +1,24 @@
-from nose.tools import *
+import pytest
 from vending_machine import VendingMachine
 
-class TestVendingMachine:
-    def test_release_change_when_no_payment_expects_0_change(self):
-        # Arrange
-        vending_machine = VendingMachine()
+def test_release_change_when_no_payment_expects_0_change():
+    # Arrange
+    vending_machine = VendingMachine()
 
-        # Act
-        result = vending_machine.release_change()
+    # Act
+    result = vending_machine.release_change()
 
-        # Assert
-        assert_true(result == 0)
-        assert_equals(0, result)
+    # Assert
+    assert result == 0
+    assert 0 == result
 
-    def test_release_change_with_payment_expects_change_returned(self):
-        # Arrange
-        vending_machine = VendingMachine()
-        vending_machine.insert_coin(1)
+def test_release_change_with_payment_expects_change_returned():
+    # Arrange
+    vending_machine = VendingMachine()
+    vending_machine.insert_coin(1)
 
-        # Act
-        result = vending_machine.release_change()
+    # Act
+    result = vending_machine.release_change()
 
-        # Assert
-        assert_true(result > 0)
-        assert_greater(result, 0)
-
-
+    # Assert
+    assert result > 0
