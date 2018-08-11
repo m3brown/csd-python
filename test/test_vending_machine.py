d09ed377bcf9abfe873a487e2ab4a8570158f9d3
Mike Brown <mike.brown@excella.com>
2018-08-10 20:09:08 -0400

    update to pytest

--- test/test_vending_machine.py
+++ test/test_vending_machine.py
@@ -1,83 +1,80 @@
-import unittest
-from nose.tools import *
+import pytest
 from vending_machine import VendingMachine
 
-class TestVendingMachine:
-    def test_release_change_when_no_payment_expects_0_change(self):
-        # Arrange
-        vending_machine = VendingMachine()
+def test_release_change_when_no_payment_expects_0_change():
+    # Arrange
+    vending_machine = VendingMachine()
 
-        # Act
-        result = vending_machine.release_change()
+    # Act
+    result = vending_machine.release_change()
 
-        # Assert
-        assert_true(result == 0)
-        assert_equals(0, result)
+    # Assert
+    assert result == 0
+    assert 0 == result
 
-    def test_release_change_with_payment_expects_change_returned(self):
-        # Arrange
-        vending_machine = VendingMachine()
-        vending_machine.insert_coin(1)
+def test_release_change_with_payment_expects_change_returned():
+    # Arrange
+    vending_machine = VendingMachine()
+    vending_machine.insert_coin(1)
 
-        # Act
-        result = vending_machine.release_change()
+    # Act
+    result = vending_machine.release_change()
 
-        # Assert
-        assert_true(result > 0)
-        assert_greater(result, 0)
+    # Assert
+    assert result > 0
 
-    @unittest.skip("buy_product now returns an exception")
-    def test_buy_product_with_no_payment_expects_nothing(self):
-        # Arrange
-        vending_machine = VendingMachine()
+@pytest.mark.skip("buy_product now returns an exception")
+def test_buy_product_with_no_payment_expects_nothing():
+    # Arrange
+    vending_machine = VendingMachine()
 
-        # Act
-        result = vending_machine.buy_product()
+    # Act
+    result = vending_machine.buy_product()
 
-        # Assert
-        assert_is_none(result)
+    # Assert
+    assert result is None
 
-    def test_buy_product_with_payment_expects_product(self):
-        # Arrange
-        vending_machine = VendingMachine()
-        vending_machine.insert_coin(1)
+def test_buy_product_with_payment_expects_product():
+    # Arrange
+    vending_machine = VendingMachine()
+    vending_machine.insert_coin(1)
 
-        # Act
-        result = vending_machine.buy_product()
+    # Act
+    result = vending_machine.buy_product()
 
-        # Assert
-        assert_is_not_none(result)
+    # Assert
+    assert result is not None
 
-    @raises(RuntimeError)
-    def test_buy_product_with_no_payment_expects_exception(self):
-        # Arrange
-        vending_machine = VendingMachine()
+def test_buy_product_with_no_payment_expects_exception():
+    # Arrange
+    vending_machine = VendingMachine()
 
-        # Act
-        result = vending_machine.buy_product()
-
-        # Assert
-        # an exception should be raised
-
-    def test_get_message_returns_success_message_with_successful_purchase(self):
-        # Arrange
-        vending_machine = VendingMachine()
-        vending_machine.insert_coin(1)
-
-        # Act
+    # Act
+    with pytest.raises(RuntimeError):
         vending_machine.buy_product()
 
+    # Assert
+    # an exception should be raised
+
+def test_get_message_returns_success_message_with_successful_purchase():
+    # Arrange
+    vending_machine = VendingMachine()
+    vending_machine.insert_coin(1)
+
+    # Act
+    vending_machine.buy_product()
+
+    # Assert
+    assert vending_machine.message == "Enjoy!"
+
+def test_get_message_returns_insert_money_message_when_purchase_fails():
+    # Arrange
+    vending_machine = VendingMachine()
+
+    # Act
+    try:
+        vending_machine.buy_product()
+        assert False, "buy_product should have failed with exception"
+    except:
         # Assert
-        assert_equals(vending_machine.message, "Enjoy!")
-
-    def test_get_message_returns_insert_money_message_when_purchase_fails(self):
-        # Arrange
-        vending_machine = VendingMachine()
-
-        # Act
-        try:
-            vending_machine.buy_product()
-            assert False, "buy_product should have failed with exception"
-        except:
-            # Assert
-            assert_equals(vending_machine.message, "Please insert money")
+        assert vending_machine.message == "Please insert money"
